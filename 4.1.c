21bfe3c4e4d13314128aacb76e27ee801ba5115e
NN0404 <100903912+NN0404@users.noreply.github.com>
2025-04-17 16:26:47 +0700

    Add files via upload

---
+++ 4.1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int max=0, min=0;
+
+
+int main (){
+	int m,n,i,j,a[50][50],c,d;
+	puts("Your m arrow value: ");
+	scanf("%d",&m);
+	puts("Your n column value: ");
+	scanf("%d",&n);
+	
+	
+for( i = 0; i<m; i++){
+	for(int j =0;j<n;j++){
+		printf("Phan tu a[%d][%d]= ",i,j );
+		scanf("%d", &a[i][j]);
+	}
+}
+for( i = 0; i<m; i++){
+	for(int j=0;j<n;j++){
+if (a[i][j]>max){ max = a[i][j];
+}
+if (a[i][j]<min){ min = a[i][j];
+}
+}}
+
+puts("Your matrix is: ");printf("%d %d \n", m,n);
+for( i = 0; i<m; i++){
+	for(int j=0;j<n;j++){
+		
+		printf("%d ",a[i][j] );
+	}
+	
+	printf("\n");
+}
+ printf("Max value is %d \n", max);
+ printf("Min value is %d", min);
+
+    return 0;
+	}
+
