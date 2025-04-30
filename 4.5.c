955d579b89712a8b7959cd43f41e61c68380c05d
NN0404 <100903912+NN0404@users.noreply.github.com>
2025-04-30 14:15:19 +0700

    Nhân ma trận

---
+++ 4.5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+    int j,k,r,t,m,n,p,q;
+    int a[50][50],b[50][50],c[50][50];
+
+    //nhap ma tran A
+    puts("Ma tran A");
+    puts("Nhap so hang m: ");
+    scanf("%d",&m);
+    puts("Nhap so cot n: ");
+    scanf("%d",&n);
+
+        for (j=1;j<=m;j++) {
+            for (k=1;k<=n;k++) {
+                printf("Phan tu a[%d][%d] la: ",j,k);
+                scanf("%d",&a[j][k]);
+            }
+        }
+printf("\n");
+    //nhap ma tran B
+    puts("Ma tran B");
+    puts("Nhap so hang p: ");
+    scanf("%d",&p);
+    puts("Nhap so cot q: ");
+    scanf("%d",&q);
+
+    for (r=1;r<=p;r++) {
+        for (t=1;t<=q;t++) {
+            printf("Phan tu b[%d][%d] la: ",r,t);
+            scanf("%d",&b[r][t]);
+        }
+    }
+
+    if (n==p) {
+        puts("Phep nhan ma tran A * B la: \n");
+        for (j=1;j<=m;j++) {
+            for (r=1;r<=q;r++) {
+                c[j][r]=0;
+                for (k=1;k<=n;k++){
+                    c[j][r] += a[j][k] * b[k][r];
+                }
+                printf("%d ",c[j][r]);
+            }
+            printf("\n");
+        }
+    }
+else {
+    puts("INVALID ");
+}
+}
